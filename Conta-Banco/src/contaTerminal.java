e8979a84da431e7e12dece1148fd2b0caff088c4
JoseGoncalvess <joseinacio774@gmail.com>
2024-08-16 11:04:50 -0300

    Projeto conta banco

---
+++ Conta-Banco/src/contaTerminal.java
@@ -0,0 +1,10 @@
+import com.conta.banco.estructure.Conta;
+
+public class contaTerminal {
+    public static void main(String[] args) {
+        Conta novaconta = new Conta();
+
+        System.out.println("Olá ," +novaconta.getName()+" obrigado por criar uma conta em nosso banco, sua agência é "+ novaconta.getAgencia() +", conta "+ novaconta.getNumero() +" e seu saldo "+ novaconta.getSaldo() +" já está disponível para saque\".");
+
+    }
+}
